ba4db81041d9c8f501926e890b879516e826c692
andrew-suh <andrew.suh.94@gmail.com>
2021-04-16 23:31:31 +0000

    Changed how I called the algo

--- sorting_algos/start.py
+++ sorting_algos/start.py
@@ -9,9 +9,9 @@ def get_problems(file_location):
     return diff_lines
 
 # Get the function from the imported file and run with list of strings
-def solve_problem(problems, algo_name):
-    getattr(bubblesort, algo_name)(problems)
+def solve_problem(problems,filename, algo_name):
+    getattr(filename, algo_name)(problems)
 
 if __name__ == "__main__":
     list_of_problems = get_problems("../problem_bank/sorting.txt")
-    solve_problem(list_of_problems, "bubblesort")
+    solve_problem(list_of_problems, bubblesort,"bubblesort")
